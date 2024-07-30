a4619a7bda3a60aba7bc6e4aabd48d1f341f95ea
zhangshuaibao <2742520302@qq.com>
2024-07-30 23:05:35 +0800

    更新

--- subarraySum.java
+++
@@ -1,8 +0,0 @@
-public class subarraySum {
-    public static void main(String[] args) {
-
-    }
-    public int subarraySum1(int[] nums, int k) {
-        return 1;
-    }
-}
---
+++ twoSum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] ints = twoSum(new int[]{3,2,4}, 6);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            //containsKey，判断key存不存在
+            if(map.containsKey(target-nums[i])){
+                //按照从0到length-1，所以小的一定会存到map里边，大的在外边判断
+                return new int[]{map.get(target-nums[i]), i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+
+    }
+
+}
