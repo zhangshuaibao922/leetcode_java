06636dd48d539f1e20260d5e6554ab57bf770ab3
zhangshuaibao <2742520302@qq.com>
2024-07-30 23:26:10 +0800

    更新

---
+++ groupAnagrams.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class groupAnagrams {
+    public static void main(String[] args) {
+
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
