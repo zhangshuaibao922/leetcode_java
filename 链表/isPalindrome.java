d4dec83687ff6e37af4a7c07d65b43d113b0e083
zhangshuaibao <2742520302@qq.com>
2024-08-13 19:46:19 +0800

    链表：1相交链表、2反转链表

---
+++ 链表/isPalindrome.java
@@ -0,0 +1,11 @@
+package 链表;
+
+public class isPalindrome {
+    public static void main(String[] args) {
+
+
+    }
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
